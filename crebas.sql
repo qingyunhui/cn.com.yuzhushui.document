1e1e97b3e5f987249c7ca34f31747a465f1f7066
卿云辉 <qingyunhui@zuozh.com>
2016-11-18 13:48:08 +0800

    update data script

--- crebas.sql
+++ crebas.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2016/11/15 23:29:28                          */
+/* Created on:     2016/11/18 13:42:18                          */
 /*==============================================================*/
 
 
@@ -44,7 +44,7 @@ create table sys_account
    comments             varchar(128) comment '备注',
    deleted              int not null comment '删除标识',
    primary key (account_id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_account comment '账号表';
 
@@ -65,7 +65,7 @@ create table sys_account_role_rel
    comments             varchar(128) comment '备注',
    deleted              int not null comment '删除标识',
    primary key (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_account_role_rel comment '账号角色关联表';
 
@@ -95,7 +95,7 @@ create table sys_attachment
    editor_id            int not null comment '修改人ID',
    deleted              int not null comment '删除标识',
    primary key (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_attachment comment '附件';
 
@@ -119,7 +119,7 @@ create table sys_dict
    comments             varchar(128) comment '备注',
    deleted              int not null comment '删除标识',
    primary key (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_dict comment '字典表';
 
@@ -146,7 +146,7 @@ create table sys_menu
    comments             varchar(128) comment '备注',
    deleted              int not null comment '是否删除',
    primary key (menu_id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_menu comment '菜单表';
 
@@ -168,7 +168,7 @@ create table sys_message
    comments             varchar(128) comment '备注',
    deleted              int not null comment '删除标识',
    primary key (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_message comment '系统消息';
 
@@ -188,7 +188,7 @@ create table sys_opration_log
    editor               varchar(16) comment '修改人',
    editor_id            int comment '修改人ID',
    deleted              int comment '删除标识'
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_opration_log comment '操作日记';
 
@@ -213,7 +213,7 @@ create table sys_resource
    comments             varchar(128) comment '备注',
    deleted              int not null comment '是否删除',
    primary key (resource_id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_resource comment '资源表';
 
@@ -236,7 +236,7 @@ create table sys_role
    comments             varchar(128) comment '备注',
    deleted              int not null comment '是否删除',
    primary key (role_id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_role comment '角色表';
 
@@ -257,7 +257,7 @@ create table sys_role_resource_rel
    comments             varchar(128) comment '备注',
    deleted              int not null comment '删除标识',
    primary key (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_role_resource_rel comment '角色资源关联表';
 
@@ -289,7 +289,7 @@ create table sys_user
    comments             varchar(128) comment '备注',
    deleted              int not null comment '删除标识',
    primary key (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 alter table sys_user comment '用户表';
 
